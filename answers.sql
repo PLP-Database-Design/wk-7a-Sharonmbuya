44089ea59849785fe72e8f94da6b6c2ea1e33afc
Sharonmbuya <sharonadhiambo558@gmail.com>
2025-05-21 11:00:06 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -18,3 +18,41 @@ VALUES
 
 
 
+
+
+
+-- Orders table
+CREATE TABLE orders(
+    OrderID INT PRIMARY KEY,
+    customerName VARCHAR(100)
+);
+
+INSERT INTO orders (OrderID, CustomerName)
+VALUES
+    (101, 'John Doe'),
+    (102, 'Jane Smith'),
+    (103, 'Emily Clark');
+
+-- Product table
+CREATE TABLE product(
+    product_id INT PRIMARY KEY,
+    productName VARCHAR(100),
+    quantity INT,
+    order_id INT,
+    FOREIGN KEY(order_id) REFERENCES orders(OrderID)
+);
+
+INSERT INTO product(product_id, productName, quantity, order_id)
+VALUES 
+    (1, 'Laptop', 2, 101),
+    (2, 'Mouse', 1, 101),
+    (3, 'Tablet', 3, 102),
+    (4, 'Keyboard', 2, 102),
+    (5, 'Mouse', 1, 102),
+    (6, 'Phone', 1, 103);
+
+
+
+
+
+
